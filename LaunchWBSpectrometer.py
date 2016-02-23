3ca7fa6178d0fd17f21d539f189eb2fa277c6ab6
User (Reber / Debian Jessie x64) <ctong@ska.ac.za>
2016-02-23 09:13:01 +0200

    Added functionality to copy bof file from the the directory of this repo to a specified path in the ROACH NFS on launch.

--- LaunchWBSpectrometer.py
+++ LaunchWBSpectrometer.py
@@ -5,6 +5,10 @@ import sys
 import time
 import socket as socket
 import struct as struct
+import os.path
+from shutil import copyfile
+import stat
+import numpy
 
 def exit_clean():
     try:
@@ -15,7 +19,10 @@ def exit_clean():
 ##### Variables to be set ###########
 
 #Gateware to be loaded.a bof should be on the ROACH and a fpg file in the same directory as this script
-gateware = 'wb_spectrometer_15_2016_Feb_17_1250'
+gateware = 'wb_spectrometer_16_2016_Feb_19_1139'
+
+#Directory on the ROACH NFS filesystem where bof files are kept. (Assumes this is hosted on this machine.)
+roachGatewareDir = '/srv/roachfs/fs/boffiles'
 
 #ROACH PowerPC Network:
 strRoachIP = 'catseye'
@@ -25,7 +32,7 @@ roachKATCPPort = 7147
 strTGbEDestinationIP = '10.0.0.4'
 tGbEDestinationPort = 60000
 
-#Set frame data length in bytes must be submultiple of 16384 ( 1024 frequencies * 2 for complex * 2 for 2 channels with each value a 4 byte uint32_t. Also excludes 8 bytes header of each frame)  
+#Set frame data length in bytes must be submultiple of 16384 ( 1024 frequencies * 2 for complex * 2 for 2 channels with each value a 4 byte uint32_t. Also excludes 8 bytes header of each frame)
 #Set interframe length clock cycles, (note 64 bytes can be transferred per cyle)
 dataSizePerPacket_B = 1024
 interpacketLength_cycles = 16
@@ -42,23 +49,47 @@ bypassPFB = 0;
 #How many FFT frames to accumulate for. Note: This is inversely proportional to output rate and time resolution and directly proportional to size of output numbers
 accumulationLength = 3125
 
-ADC_attenuation = 0
+ADCAttenuation = 0
+
+#Threshold detection for ADC to ensure input signal is in the required range
+lowerADCThreshold = 1000
+upperADCThreshold = 100000
+ADCThresholdAccumLength = 1000 #Note accumulation is done with every 4th sample at 800 MSps
 
 ####################################
 
 packedIP = socket.inet_aton(strTGbEDestinationIP)
 tGbEDestinationIP = struct.unpack("!L", packedIP)[0]
 
+powerPerADCValue_mW = pow(1.9 / pow(2, 8), 2) / 50 * 1000 #V*V/R to get power with 1.9V across 8 bits into 50 Ohm impedance.
+
 print '\n---------------------------'
 print 'Configuration:'
 print '---------------------------'
-print ' FPGA gateware:         ', gateware
-print ' Destination 10GbE host:', strTGbEDestinationIP, '( ', tGbEDestinationIP, ' )'
-print ' Data size per packet:  ', dataSizePerPacket_B, ' bytes'
-print ' Interpacket length     ', interpacketLength_cycles, ' cycles'
-print ' FFT shift mask         ', coarseFFTShiftMask
-print ' Accumulation length    ', accumulationLength, '(', 2048 * accumulationLength / 800e3, ' ms integration per output )' 
+print ' FPGA gateware:			', gateware
+print ' Gateware directory		', roachGatewareDir
+print ' Destination 10GbE host:		', strTGbEDestinationIP, '( ', tGbEDestinationIP, ' )'
+print ' Data size per packet:		', dataSizePerPacket_B, ' bytes'
+print ' Interpacket length		', interpacketLength_cycles, ' cycles'
+print ' FFT shift mask			', coarseFFTShiftMask
+print ' Accumulation length		', accumulationLength, '(', 2048 * accumulationLength / 800e3, ' ms integration per output )'
+print ' ADC attenuation			', ADCAttenuation, '(', ADCAttenuation / 2, ' dB )'
+print ' ADC upper threshold		', lowerADCThreshold, '(', 10 * numpy.log10( powerPerADCValue_mW * lowerADCThreshold / ADCThresholdAccumLength ), ' dBm at ADC input )'
+print ' ADC lower threshold		', upperADCThreshold, '(', 10 * numpy.log10( powerPerADCValue_mW * upperADCThreshold / ADCThresholdAccumLength ), ' dBm at ADC input )'
+print ' ADC thres accumulation length	', ADCThresholdAccumLength, '(', ADCThresholdAccumLength / 50, ' dB )'
 print '---------------------------'
+
+print '\n---------------------------'
+print 'Checking gateware...'
+if not( roachGatewareDir.endswith('/') ):
+  roachGatewareDir += '/'
+
+if os.path.isfile(roachGatewareDir + gateware + '.bof'):
+  print 'Found bof file:', gateware + '.bof'
+else:
+  print 'Copying bof file', gateware + '.bof', 'to NFS (' +  roachGatewareDir + ')'
+  copyfile(gateware + '.bof', roachGatewareDir + gateware + '.bof')
+  os.chmod(roachGatewareDir + gateware + '.bof', stat.S_IXUSR | stat.S_IXGRP |  stat.S_IXOTH)
 
 print '\n---------------------------'
 print 'Connecting to FPGA...'
@@ -70,21 +101,21 @@ else:
         print 'ERROR connecting to KATCP server.'
         exit_clean()
 
-print 'Flashing gateware'  
+print 'Flashing gateware'
 
 fpga.system_info['program_filename'] = '%s.bof' % gateware #bof needs to be on the roachfs for this to work
 fpga.program()
 fpga.get_system_information('%s.fpg' % gateware)
 sys.stdout.flush()
 
-print '\n---------------------------'    
+print '\n---------------------------'
 print 'Setting destination network host...'
 
 fpga.registers.tgbe0_dest_ip.write(reg = tGbEDestinationIP)
 fpga.registers.tgbe0_dest_port.write(reg = tGbEDestinationPort)
 sys.stdout.flush()
 
-print '\n---------------------------'    
+print '\n---------------------------'
 print 'Checking 10 Gb Ethernet link state...'
 
 time.sleep(2) # Wait 2 seconds for 10GbE link to come up
@@ -101,7 +132,7 @@ fpga.registers.eth_packets_per_accum_window.write_int(16384 / dataSizePerPacket_
 print '10 Gb link is up.'
 sys.stdout.flush()
 
-print '\n---------------------------'    
+print '\n---------------------------'
 print 'Setting FFT shift and accumulation length.'
 #Setup values for WB spectrum output
 fpga.registers.coarse_fft_shift_mask.write_int(coarseFFTShiftMask) #Shift for each FFT stage (2048 points -> 11 stages. Value is a bit mask so decimal of 11111111111. i.e. 2047)
@@ -115,15 +146,21 @@ print '\n---------------------------'
 print 'Enabling/disabling PFB bypass.'
 fpga.registers.bypass_pfb.write_int(bypassPFB)
 
-print '\n---------------------------'    
+print '\n---------------------------'
 print 'Enabling ADCs and setting attentuation.'
 #Enable the ADCs
 fpga.registers.adc0_en.write_int(1)
 fpga.registers.adc1_en.write_int(1)
-fpga.registers.adc0_atten.write_int(ADC_attenuation)
-fpga.registers.adc1_atten.write_int(ADC_attenuation)
+fpga.registers.adc0_atten.write_int(ADCAttenuation)
+fpga.registers.adc1_atten.write_int(ADCAttenuation)
 
-print '\n---------------------------'    
+print '\n---------------------------'
+print 'Setting up ADC threshold notification.'
+fpga.registers.upper_adc_threshold.write_int(upperADCThreshold)
+fpga.registers.lower_adc_threshold.write_int(lowerADCThreshold)
+fpga.registers.adc_threshold_acc_length.write_int(ADCThresholdAccumLength)
+
+print '\n---------------------------'
 print 'Configuring noise diode.'
 fpga.registers.noise_diode_on_length.write_int(125) #Set noise diode duty-cycle in accumulation windows (note values can't be 0 will default to 1)
 fpga.registers.noise_diode_off_length.write_int(7375)
@@ -131,7 +168,7 @@ fpga.registers.noise_diode_off_length.write_int(7375)
 fpga.registers.noise_diode_duty_cycle_en.write_int(1) #Noise diode mode: always on (0) or duty-cycle (1) as set above.
 fpga.registers.noise_diode_en.write_int(1) #Global enabling or disabling of noise diode
 
-print '\n---------------------------'    
+print '\n---------------------------'
 print 'Setting RTC and signal board to resync on next PPS pulse...'
 
 #Check clock frequency
@@ -171,5 +208,5 @@ else:
   print 'Offset < 1 s. Time primed correctly.'
 
 
-print '\n---------------------------'    
+print '\n---------------------------'
 print 'Done'
