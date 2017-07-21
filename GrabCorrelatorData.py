Seems fixed, let's try to resolve X-scaling.

GrabCorrelatorData.py
@@ -43,6 +43,16 @@ class SubplotAnimation(animation.TimedAnimation):
         self.ax_10_ri.set_title("1x0 real/imag")
         self.ax_11_ri.set_title("1x1 real/imag")
 
+        self.ax_00_m.set_xlim(0,2048)
+        self.ax_01_m.set_xlim(0,2048)
+        self.ax_10_m.set_xlim(0,2048)
+        self.ax_11_m.set_xlim(0,2048)
+
+        self.ax_00_ri.set_xlim(0,2048)
+        self.ax_01_ri.set_xlim(0,2048)
+        self.ax_10_ri.set_xlim(0,2048)
+        self.ax_11_ri.set_xlim(0,2048)
+
         self.line_00_m, = self.ax_00_m.plot([], [], color="blue", label="mag")
         self.line_01_m, = self.ax_01_m.plot([], [], color="blue", label="mag")
         self.line_10_m, = self.ax_10_m.plot([], [], color="blue", label="mag")
@@ -94,9 +104,12 @@ class SubplotAnimation(animation.TimedAnimation):
 
         self.ax_00_m.set_ylim(np.min(p00_dB) - 0.1*(np.max(p00_dB) - np.min(p00_dB)),
                               np.max(p00_dB) + 0.1*(np.max(p00_dB) - np.min(p00_dB)))
-        self.ax_01_m.relim()
-        self.ax_10_m.relim()
-        self.ax_11_m.relim()
+        self.ax_01_m.set_ylim(np.min(p01_dB) - 0.1*(np.max(p01_dB) - np.min(p01_dB)),
+                              np.max(p01_dB) + 0.1*(np.max(p01_dB) - np.min(p01_dB)))
+        self.ax_10_m.set_ylim(np.min(p10_dB) - 0.1*(np.max(p10_dB) - np.min(p10_dB)),
+                              np.max(p10_dB) + 0.1*(np.max(p10_dB) - np.min(p10_dB)))
+        self.ax_11_m.set_ylim(np.min(p11_dB) - 0.1*(np.max(p11_dB) - np.min(p11_dB)),
+                              np.max(p11_dB) + 0.1*(np.max(p11_dB) - np.min(p11_dB)))
 
         self.line_00_p.set_data(self.f, np.degrees(np.angle(p00)))
         self.line_01_p.set_data(self.f, np.degrees(np.angle(p01)))